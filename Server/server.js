fdcda19873e3518b4cb0d4c9cde7a27d651f5047
minas <minasanton@gmail.com>
2018-01-26 00:09:23 -0800

    added CORS

--- Server/server.js
+++ Server/server.js
@@ -10,6 +10,11 @@ app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api/v1', routes);
 
 app.listen(3000, function(){
